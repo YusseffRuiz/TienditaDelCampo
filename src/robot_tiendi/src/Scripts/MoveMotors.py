6b50a3494481565ca966480ef02cf9670a2e59d5
YusseffRuiz <zeiverking@gmail.com>
2020-03-30 04:28:03 +0100

    modifications

--- src/robot_tiendi/src/Scripts/MoveMotors.py
+++ src/robot_tiendi/src/Scripts/MoveMotors.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python
 
-import rospy
+#import rospy
 import sys
 import time
 import RPi.GPIO as GPIO
 
 import math
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from tf.transformations import euler_from_quaternion
-from geometry_msgs.msg import Point, Twist
+#from nav_msgs.msg import Odometry
+#from geometry_msgs.msg import PoseWithCovarianceStamped
+#from tf.transformations import euler_from_quaternion
+#from geometry_msgs.msg import Point, Twist
 
 
 
@@ -19,33 +19,32 @@ class MoveMotors:
 
         # Creates a node with name 'speed_controller' and make sure it is a
         # unique node (using anonymous=True).
-        rospy.init_node("move_motors", anonymous=True)
+ #       rospy.init_node("move_motors", anonymous=True)
         # Publisher which will publish to the topic '/cmd_vel'.
         self.mode = GPIO.getmode()
-        GPIO.cleanup()
         self.LeftMotorP = 26
         self.pwmL = 13
         self.LeftMotorN = 19
         self.RightMotorP = 20
         self.pwmR = 21
         self.RightMotorN = 16
-        self.r = rospy.Rate(4)
+#        self.r = rospy.Rate(4)
 
 
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.RightMotorF, GPIO.OUT)
-        GPIO.setup(self.RightMotorB, GPIO.OUT)
-        GPIO.setup(self.LeftMotorF, GPIO.OUT)
-        GPIO.setup(self.LeftMotorB, GPIO.OUT)
+        GPIO.setup(self.RightMotorP, GPIO.OUT)
+        GPIO.setup(self.RightMotorN, GPIO.OUT)
+        GPIO.setup(self.LeftMotorP, GPIO.OUT)
+        GPIO.setup(self.LeftMotorN, GPIO.OUT)
         GPIO.setup(self.pwmL, GPIO.OUT)
         GPIO.setup(self.pwmR, GPIO.OUT)
 
-        self.MotorLeftSpeed = GPIO.PWM =(self.pwmL, 100)
-        self.MotorRightSpeed = GPIO.PWM = (self.pwmR, 100)
-        self.MotorLeftSpeed.start()
-        self.MotorRightSpeed.start()
-        rospy.loginfo("GPIOs cleaned and assigned")
+        self.MotorLeftSpeed = GPIO.PWM(self.pwmL, 100)
+        self.MotorRightSpeed = GPIO.PWM(self.pwmR, 100)
+        self.MotorLeftSpeed.start(1)
+        self.MotorRightSpeed.start(1)
+  #      rospy.loginfo("GPIOs cleaned and assigned")
 
     def set(self, direction, value):
         if(direction == 'L'):
@@ -61,7 +60,7 @@ class MoveMotors:
         GPIO.output(self.LeftMotorP, GPIO.HIGH)
         GPIO.output(self.LeftMotorN, GPIO.LOW)
         print("Moving Forward")
-        self.r.sleep()
+#        self.r.sleep()
 
     def reverse(self):
         GPIO.output(self.RightMotorN, GPIO.HIGH)
@@ -69,18 +68,26 @@ class MoveMotors:
         GPIO.output(self.LeftMotorN, GPIO.HIGH)
         GPIO.output(self.LeftMotorP, GPIO.LOW)
         print("Moving Backwards")
-        self.r.sleep()
+ #       self.r.sleep()
 
     def stopMotors(self):
         self.MotorRightSpeed.stop()
         self.MotorLeftSpeed.stop()
         GPIO.cleanup()
+	print("Stopping Motors")
 
 
 if __name__ == "__main__":
     try:
-        x = MoveMotors()
-        x.forward()
-        x.stopMotors()
-    except rospy.ROSInterruptException:
-        pass
+	x = MoveMotors()
+	x.forward()
+	while(True):
+           # x.forward()
+            x.set('L', 50)
+	    x.set('R', 50)
+	#x.stopMotors()
+    except KeyboardInterrupt:
+	x.stopMotors()
+	pass
+#    except rospy.ROSInterruptException:
+#        pass

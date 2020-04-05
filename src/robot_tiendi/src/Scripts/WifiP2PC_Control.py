c73c736979e73ad70fc28c14e1c24da6bb7bf417
YusseffRuiz <zeiverking@gmail.com>
2020-04-06 00:20:25 +0100

    Create WifiP2P control code

--- src/robot_tiendi/src/Scripts/WifiP2PC_Control.py
+++ src/robot_tiendi/src/Scripts/WifiP2PC_Control.py
@@ -6,7 +6,7 @@ def start_server_program():
     host = socket.gethostname()
     port = 8888
     server_socket = socket.socket()
-    server_socket.bind(host, port)
+    server_socket.bind((host, port))
 
     server_socket.listen(2)
     conn, address = server_socket.accept()

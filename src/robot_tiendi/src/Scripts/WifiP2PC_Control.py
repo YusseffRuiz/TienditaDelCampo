a54de0b432f94bd86af0747b0161fe7da9299730
YusseffRuiz <zeiverking@gmail.com>
2020-04-07 02:32:24 +0100

    Trying with py2py library

--- src/robot_tiendi/src/Scripts/WifiP2PC_Control.py
+++ src/robot_tiendi/src/Scripts/WifiP2PC_Control.py
@@ -1,4 +1,3 @@
-
 import socket
 import subprocess
 
@@ -13,51 +12,28 @@ def cmd_p2p_pi(cmd):
 def start_server_program():
     host = socket.gethostname()
     port = 8888
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(('127.0.0.1', port))
-        s.listen(5)
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                print(data)
-                if not data:
-                    break
-    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # server_socket.bind(('127.0.0.1', port))
-    # print("Waiting for a connection, host: " + host + ", port: " + str(port))
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('127.0.0.1', port))
+    print("Waiting for a connection, host: " + host + ", port: " + str(port))
 
-    # server_socket.listen(2)
-    # conn, address = server_socket.accept()
-    # print("connection From: " + str(address))
-    # return conn, address
+    server_socket.listen(2)
+    conn, address = server_socket.accept()
+    print("connection From: " + str(address))
+    return conn, address
 
 
 if __name__ == "__main__":
     cmd_p2p_pi("find")
     try:
-        port = 8888
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(('127.0.0.1', port))
-            s.listen(5)
-            conn, addr = s.accept()
-            with conn:
-                print('Connected by', addr)
-                while True:
-                    data = conn.recv(1024)
-                    print(data)
-                    if not data:
-                        break
-        # conn, address = start_server_program()
-        # try:
-        #     while True:
-        #         data = conn.recv(4096).decode()
-        #         if data:
-        #             print("From connected user: " + str(data))
-        # except KeyboardInterrupt:
-        #     conn.close()
-        #     pass
+        conn, address = start_server_program()
+        try:
+            while True:
+                data = conn.recv(4096).decode()
+                if data:
+                    print("From connected user: " + str(data))
+        except KeyboardInterrupt:
+            conn.close()
+            pass
     except KeyboardInterrupt:
         pass
     finally:

e3148c76d9ac5a36823902d2e24b7f431ff0bfda
YusseffRuiz <zeiverking@gmail.com>
2020-04-07 01:58:52 +0100

    modifying WifiP2P

--- src/robot_tiendi/src/Scripts/WifiP2PC_Control.py
+++ src/robot_tiendi/src/Scripts/WifiP2PC_Control.py
@@ -6,7 +6,8 @@ def cmd_p2p_pi(cmd):
     command = "p2p_" + cmd
     p = subprocess.Popen(["wpa_cli", command], stdout=subprocess.PIPE)
     output, err = p.communicate()
-    print("*** Running wpa command ***\n", output)
+    print("*** Running wpa command ***")
+    print(output)
 
 def start_server_program():
     host = socket.gethostname()
@@ -23,13 +24,16 @@ def start_server_program():
 
 if __name__ == "__main__":
     cmd_p2p_pi("find")
-    conn, address = start_server_program()
     try:
+        conn, address = start_server_program()
         while True:
             data = conn.recv(4096).decode()
             if data:
                 print("From connected user: " + str(data))
     except KeyboardInterrupt:
+        conn.close()
+        cmd_p2p_pi("stop")
+        print("Ending Program, closing connection")
         pass
     finally:
         conn.close()

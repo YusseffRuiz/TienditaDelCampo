50aaea711bf9272126dffa0691df9c8ec0cc3b8c
YusseffRuiz <zeiverking@gmail.com>
2020-04-07 02:25:31 +0100

    Trying with py2py library

--- src/robot_tiendi/src/Scripts/WifiP2PC_Control.py
+++ src/robot_tiendi/src/Scripts/WifiP2PC_Control.py
@@ -12,28 +12,40 @@ def cmd_p2p_pi(cmd):
 def start_server_program():
     host = socket.gethostname()
     port = 8888
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    print("Waiting for a connection, host: " + host + ", port: " + str(port))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('127.0.0.1', port))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                print(data)
+                if not data:
+                    break
+    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # server_socket.bind(('127.0.0.1', port))
+    # print("Waiting for a connection, host: " + host + ", port: " + str(port))
 
-    server_socket.listen(2)
-    conn, address = server_socket.accept()
-    print("connection From: " + str(address))
-    return conn, address
+    # server_socket.listen(2)
+    # conn, address = server_socket.accept()
+    # print("connection From: " + str(address))
+    # return conn, address
 
 
 if __name__ == "__main__":
     cmd_p2p_pi("find")
     try:
-        conn, address = start_server_program()
-        try:
-            while True:
-                data = conn.recv(4096).decode()
-                if data:
-                    print("From connected user: " + str(data))
-        except KeyboardInterrupt:
-            conn.close()
-            pass
+        start_server_program()
+        # conn, address = start_server_program()
+        # try:
+        #     while True:
+        #         data = conn.recv(4096).decode()
+        #         if data:
+        #             print("From connected user: " + str(data))
+        # except KeyboardInterrupt:
+        #     conn.close()
+        #     pass
     except KeyboardInterrupt:
         pass
     finally:
